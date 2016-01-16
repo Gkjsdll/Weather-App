9627c48ceb1d962200397138fb6bcdac67e39d7c
Zack Winchell <zackwinchell@gmail.com>
2016-01-16 03:48:52 -0800

    redid some of the page layout

--- main.js
+++ main.js
@@ -4,27 +4,64 @@ $(document).ready(init);
 
 var $locationInput;
 var $locationInputBtn;
+var $times12hour;
+var $temps12hour;
+var $day12hour1;
 
 var assumedLocation;
-
-$.get("http://api.wunderground.com/api/055d327551912b82/geolookup/q/94107.json")
-    .done(function(data){
-    assumedLocation = data.location;
-    $locationInput.val(assumedLocation.city + ", " + assumedLocation.state);
-});
-
-
+var forecast;
 
 function updateLocation(){
-  //get values from user input
   console.log("Clicky Clicky");
+  zip12hour();
 };
 
 
+function init(){
+  $locationInput = $('#locationInput');
+  $locationInputBtn = $('#locationInputBtn');
+  $times12hour = $('#times12hour').children();
+  $temps12hour = $('#temps12hour').children();
+  $day12hour1 = $('#day12hour1');
 
-  function init(){
-    $locationInput = $('#locationInput');
-    $locationInputBtn = $('#locationInputBtn');
-    $locationInputBtn.click(updateLocation);
-    $locationInput.attr("value", "test");
+  $locationInputBtn.click(updateLocation);
+
+  $.get("http://api.wunderground.com/api/055d327551912b82/geolookup/q/autoip.json")
+  .done(function(data){
+    assumedLocation = data.location;
+    $locationInput.val(assumedLocation.city + ", " + assumedLocation.state);
+    $locationInput.val("");
+    zip12hour();
+  });
+  severeConditions();
+}
+
+function zip12hour(){
+  $.get("http://api.wunderground.com/api/055d327551912b82/hourly/q/" + $locationInput.val() + ".json")
+  .done(function(data){
+    forecast = data.hourly_forecast;
+    if(forecast[0].FCTTIME.weekday_name !== forecast[11].FCTTIME.weekday_name){
+      $day12hour1.text(forecast[0].FCTTIME.weekday_name + " – " + forecast[11].FCTTime.weekday_name);
+    }
+    else{
+      $day12hour1.text(forecast[0].FCTTIME.weekday_name);
+    }
+    for(var hour in forecast.splice(0, 12)){
+      $times12hour.eq(hour).text(forecast[hour].FCTTIME.civil);
+      $temps12hour.eq(hour).text(forecast[hour].temp.metric + "\u00B0c");
+    };
+  });
+  console.log("zip12hour executed");
+}
+
+function severeConditions(){
+  $.get("http://api.wunderground.com/api/055d327551912b82/currenthurricane/view.json")
+  .done(function(data){
+    debugger;
+    for(var condition in data.currenthurricane){
+      var conditionName = data.currenthurricane[condition].stormInfo.stormName_Nice;
+
+    }
+  });
+  console.log("severeConditions executed");
 }
