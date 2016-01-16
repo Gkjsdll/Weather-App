03abd9e9cb2812fe0c9616559fd6347f9ce2db3a
Zack Winchell <zackwinchell@gmail.com>
2016-01-16 04:33:30 -0800

    finished current hurricanes, added humidity

--- main.js
+++ main.js
@@ -7,14 +7,11 @@ var $locationInputBtn;
 var $times12hour;
 var $temps12hour;
 var $day12hour1;
+var $severeConditions;
+var $humidity;
 
-var assumedLocation;
 var forecast;
 
-function updateLocation(){
-  console.log("Clicky Clicky");
-  zip12hour();
-};
 
 
 function init(){
@@ -23,18 +20,23 @@ function init(){
   $times12hour = $('#times12hour').children();
   $temps12hour = $('#temps12hour').children();
   $day12hour1 = $('#day12hour1');
+  $severeConditions = $('#severeConditions');
+  $humidity = $('#humidity');
 
   $locationInputBtn.click(updateLocation);
 
   $.get("http://api.wunderground.com/api/055d327551912b82/geolookup/q/autoip.json")
   .done(function(data){
-    assumedLocation = data.location;
-    $locationInput.val(assumedLocation.city + ", " + assumedLocation.state);
-    $locationInput.val("");
-    zip12hour();
+    $locationInput.val(data.location.zip);
+    updateLocation();
   });
-  severeConditions();
 }
+
+function updateLocation(){
+  zip12hour();
+  severeConditions();
+  humidity();
+};
 
 function zip12hour(){
   $.get("http://api.wunderground.com/api/055d327551912b82/hourly/q/" + $locationInput.val() + ".json")
@@ -51,17 +53,33 @@ function zip12hour(){
       $temps12hour.eq(hour).text(forecast[hour].temp.metric + "\u00B0c");
     };
   });
-  console.log("zip12hour executed");
 }
 
 function severeConditions(){
+  $severeConditions.children().remove();
   $.get("http://api.wunderground.com/api/055d327551912b82/currenthurricane/view.json")
   .done(function(data){
-    debugger;
     for(var condition in data.currenthurricane){
       var conditionName = data.currenthurricane[condition].stormInfo.stormName_Nice;
-
+      var conditionObj = $('<p>').text(conditionName);
+      $severeConditions.append(conditionObj);
     }
   });
-  console.log("severeConditions executed");
+}
+
+function humidity(){
+  $humidity.children().remove();
+  $.get("http://api.wunderground.com/api/055d327551912b82/conditions/q/CA/San_Francisco.json")
+  .done(function(data){
+    var relStr = "Relative humidity: " + data.current_observation.relative_humidity;
+    var relObj = $('<p>').text(relStr);
+
+    var humidityStr = "Feels like: " + data.current_observation.feelslike_string;
+    var humidityObj = $('<p>').text(humidityStr);
+
+    var dewPointStr = "Dewpoint: " + data.current_observation.dewpoint_string;
+    var dewPointObj = $('<p>').text(dewPointStr);
+
+    $humidity.append(relObj, humidityObj, dewPointObj);
+  });
 }
